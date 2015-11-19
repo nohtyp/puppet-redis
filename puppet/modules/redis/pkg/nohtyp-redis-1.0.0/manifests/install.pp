2ccbe85e20a095f0a322cc60b3368875cdf4a20f
Thomas Foster <thomas.foster@noaa.gov>
2015-11-19 00:29:36 -0500

    updated readme

--- puppet/modules/redis/pkg/nohtyp-redis-1.0.0/manifests/install.pp
+++
@@ -1,6 +0,0 @@
-class redis::install inherits params {
-
-  package { $::redis::redis_package:
-    ensure    => installed,
-  }
-}
--- puppet/modules/redis/pkg/nohtyp-redis-1.0.0/spec/classes/init_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-describe 'redis' do
-
-  context 'with defaults for all parameters' do
-    it { should contain_class('redis') }
-  end
-end
